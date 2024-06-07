83163864972d13859a58f19cff8344253c14a948
Drewpl2021 <linoplas0@gmail.com>
2024-06-07 16:39:30 -0500

    dfadsf

--- ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
+++ ms-registry-server/src/main/java/com/example/msregistryserver/MsRegistryServerApplication.java
@@ -3,6 +3,8 @@ package com.example.msregistryserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class MsRegistryServerApplication {
 
