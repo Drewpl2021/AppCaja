263877e1a663346853527558b3dd1cfe095eecb0
jherson91fs <jherson.fernandez.19@gmail.com>
2024-06-09 00:49:08 -0500

    dsffgf

--- AngularCaja/src/app/app.module.ts
+++ AngularCaja/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layouts/angular/navbar/navbar.component';
+import { RedesComponent } from './components/layouts/angular/redes/redes.component';
+import { FoooterComponent } from './components/layouts/angular/foooter/foooter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RedesComponent,
+    FoooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ AngularCaja/src/app/components/layouts/angular/foooter/foooter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foooter',
+  templateUrl: './foooter.component.html',
+  styleUrls: ['./foooter.component.scss']
+})
+export class FoooterComponent {
+
+}
