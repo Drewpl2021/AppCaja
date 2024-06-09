1ad4021bbe41b35c220dda06c4e33d1b7434ea06
Drewpl2021 <linoplas0@gmail.com>
2024-06-08 23:22:10 -0500

    jhgafa

--- ms-fyv/src/main/java/com/example/msfyv/util/PdfUtils.java
+++ ms-fyv/src/main/java/com/example/msfyv/util/PdfUtils.java
@@ -21,39 +21,46 @@ public class PdfUtils {
         table.setWidthPercentage(100);
         Font smallFont = new Font(Font.FontFamily.HELVETICA, 8, Font.NORMAL);
         Font largeFont = new Font(Font.FontFamily.HELVETICA, 10, Font.NORMAL);
-        Font boldFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+        Font boldFont = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);
         table.addCell(new PdfPCell(new Phrase("Codigo", largeFont)));
         table.addCell(new PdfPCell(new Phrase("Descripcion", largeFont)));
         table.addCell(new PdfPCell(new Phrase("UM", largeFont)));
         table.addCell(new PdfPCell(new Phrase("Cantidad", largeFont)));
         table.addCell(new PdfPCell(new Phrase("Valor Unitario", largeFont)));
-        table.addCell(new PdfPCell(new Phrase("Valor Descuento", largeFont)));
+        table.addCell(new PdfPCell(new Phrase("Valor IGV", largeFont)));
         table.addCell(new PdfPCell(new Phrase("Total", largeFont)));
         float[] columnWidths = {1f, 3f, 1f, 1f, 1f, 1f, 1f};
-        table.setWidths(columnWidths);
+        table.setWidths(columnWidths);PdfPTable singleColumnTable = new PdfPTable(1);
+        for (int i = 0; i < 1; i++) {
+            singleColumnTable.addCell("Celda ");
+        }
+
 
         // Write column names
         // Map<String, Object> firstRow = queryResults.get(0);
         for (Factura factura : facturas) {
-
-            document.add(new Paragraph(" GASOLINERA INKAPACARITA ", boldFont));
+            document.add(new Paragraph("GASOLINERA INKAPACARITA ", boldFont));
             document.add(new Paragraph("Av. Las Flores 123, Piso 8, Edificio Central, Lima, Perú", smallFont));
             document.add(new Paragraph("contacto@tecnosoluciones.com.pe", smallFont));
             document.add(new Paragraph("  ", boldFont));
-            document.add(new Paragraph("CLIENTE                       :"+factura.getClienteId() +"                                                 " + "FECHA DE EMISION           :  "+  factura.getFecha_hora(), largeFont));
+            document.add(new Paragraph("  ", boldFont));
+            document.add(singleColumnTable);
+        }
+        for (Factura factura : facturas) {
+            document.add(new Paragraph("CLIENTE                       :"+factura.getClienteId() +"                                                 " + "FECHA DE EMISION                      :  "+  factura.getFecha_hora(), largeFont));
             document.add(new Paragraph("DOC: IDENTIDAD         :"+factura.getClienteId() + "                                                 " +"ORAGANIZACION DE VENTAS    : GRIFO INKAPACARITA", largeFont));
-            document.add(new Paragraph("DIRECCION                  :"+factura.getClienteId() + "                                                 "+"TIPO DE MODENA             : SOL", largeFont));
+            document.add(new Paragraph("DIRECCION                  :"+factura.getClienteId() + "                                                 "+"TIPO DE MODENA                         : SOL", largeFont));
 
             document.add(new Paragraph("  ", boldFont));
             document.add(table);
-            document.add(new Paragraph(""+factura.getId(), largeFont));
-            document.add(new Paragraph("Fecha y hora: ", largeFont));
-            document.add(new Paragraph("Cantidad: " + factura.getCantidad(), largeFont));
-            document.add(new Paragraph("Precio base total: " + factura.getPrecioBaseTotal(), largeFont));
-            document.add(new Paragraph("IGV: " + factura.getIgv(), largeFont));
-            document.add(new Paragraph("Total: " + factura.getTotal(), largeFont));
-            document.add(new Paragraph("Producto ID: " + factura.getProductoId(), largeFont));
-            document.add(new Paragraph("Cliente ID: " + factura.getClienteId(), largeFont));
+            document.add(new Paragraph("          "+factura.getId()   +
+                    "                                                    "+factura.getProductoId()+
+                    "                        "+"NIU"+
+                    "              "+factura.getCantidad()+
+                    "                  "+factura.getProductoId()+
+                    "                 "+factura.getIgv()+
+                    "                "+factura.getTotal(), largeFont));
+
 
 
         }
