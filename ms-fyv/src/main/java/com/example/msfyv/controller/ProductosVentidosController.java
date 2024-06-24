dce43444d760f7c73dcd8843fa64a8d371255eab
Drewpl2021 <linoplas0@gmail.com>
2024-06-23 21:29:19 -0500

    Lyufh

--- ms-fyv/src/main/java/com/example/msfyv/controller/ProductosVentidosController.java
+++ ms-fyv/src/main/java/com/example/msfyv/controller/ProductosVentidosController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/factura")
+@RequestMapping("/productosVendidos")
 public class ProductosVentidosController {
     @Autowired
     private ProductosVendidosService productosVendidosService;
