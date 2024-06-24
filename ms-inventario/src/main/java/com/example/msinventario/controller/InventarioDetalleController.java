487e060d7f4c192534a69eacb61a8cf5f1341ce3
Alessandro Pastor <alexkip159alex@gmail.com>
2024-06-23 21:16:47 -0500

    commit

--- ms-inventario/src/main/java/com/example/msinventario/controller/InventarioDetalleController.java
+++ ms-inventario/src/main/java/com/example/msinventario/controller/InventarioDetalleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/inventario-detalle")
+@RequestMapping("/detalle")
 public class InventarioDetalleController {
     @Autowired
     private InventarioDetalleService inventarioDetalleService;
